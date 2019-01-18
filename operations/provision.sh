9cf9f5fe9a208bf6dbac64db5d5ac63ca7943e92
Greg Wiley <gwiley@cj.com>
2019-01-18 07:18:30 -0800

    responder infra

---
+++ operations/provision.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+product='authorization-service'
+
+
+operation() {
+    local stack_name="$1"
+    aws cloudformation describe-stacks --stack-name "$stack_name" &> /dev/null
+    if [ "$?" -eq 0 ]; then
+        echo -n update
+    else
+        echo -n create
+    fi
+}
+
+provision() {
+    local stack_name="${product}-${1}"
+    local config_file="cf-${1}.yaml"
+    local op="$(operation $stack_name)"
+    aws cloudformation "${op}-stack" \
+        --stack-name $stack_name \
+        --template-body "file://${config_file}" \
+        --capabilities CAPABILITY_IAM \
+    || echo "$stack_name not provisioned: $?" 1>&2
+}
+
+cd "$(dirname "$0")"
+
+export AWS_DEFAULT_REGION=us-east-1
+
+# Infrastructure
+provision responder
